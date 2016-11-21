5fd953ceb4b0a6dc0c1cf5edd273442fe4cdb0c3
Jason McCoy <jasonmccoy@Jasons-MacBook-Pro.local>
2016-11-21 01:40:53 -0800

    Displays a user’s photo library using Visual Studio for Mac
    I’m using Visual Studio for Mac (Xamarin) to create an iPhone app that
    displays a user’s photo library.

---
+++ CollectionViewXamarin/PhotoCollectionImageCell.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace CollectionViewXamarin
+{
+    public partial class PhotoCollectionImageCell : UICollectionViewCell
+    {
+        public PhotoCollectionImageCell (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
